d2202fc646b0fd8b3e9708b68ab37ce604eb11f4
celloworld <camerondonahue@gmail.com>
2017-12-26 18:28:52 -0600

    rm node_modules
    removed node_modules, blew some stuff up, not much to show yet

--- app.js
+++ app.js
@@ -4,22 +4,78 @@ var express = require('express'),
     MongoClient = require('mongodb').MongoClient,
     bodyParser = require('body-parser');
     assert = require('assert');
+    moment = require('moment');
     shortid = require('shortid');
-    userID = userID ? userID : shortid.generate();
-    sessionEvents = [] 
+    userID = shortid.generate();
+    sessionEvents = [];
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-window.onload = function() {
-    eventHandling();
 
-    document.onmousemove = handleMouseMove;
-
-
+// Handler for internal server errors
+function errorHandler(err, req, res, next) {
+    console.error(err.message);
+    console.error(err.stack);
+    res.status(500).render('error_template', { error: err });
 }
+
+MongoClient.connect('mongodb://localhost:27017/a', function(err, db) {
+
+    assert.equal(null, err, "error connecting to MongoClient @ __dirname");
+    console.log("Successfully connected to MongoDB... sweeeet");
+
+
+
+    app.get('/', function(req, res, next) {
+        // window.onload = function() {
+            // document.onmousemove = handleMouseMove;
+        // }
+        var today = moment().date().toString();
+        console.log(today);
+        // TODO: compare incoming user profile against preceding profiles
+        // if user exists, get id, insert visit date and duration to user profile
+        
+        // else generate userID
+        userID = userID || shortid.generate();
+        
+        
+        res.render('index', { 'userID' : userID });
+        // window.onbeforeunload = function() {
+        //     alert('hi');
+        //     db.collection(today).insert(sessionEvents);
+        // }
+    });
+
+    // app.post('/movie-entered', function(req, res, next) {
+    //     var movie = {
+    //         "imdb": req.body.imdb,
+    //         "title": req.body.title,
+    //         "year": req.body.year
+    //     }
+    //     console.log(req.body);
+    //     console.log(movie, typeof movie.imdb, typeof movie.title, typeof movie.year, typeof movie);
+    //     if (typeof movie.imdb != 'string' || typeof movie.title != 'string' || typeof movie.year != 'string') {
+    //         next('Please fill out the form completely!');
+    //     }
+    //     else {
+    //         db.collection('movies').insert(movie);
+    //         res.sendFile('views/success.html', {root: __dirname });
+
+    //     }
+    // });
+    
+    app.use(errorHandler);
+
+    var server = app.listen(3000, function() {
+        var port = server.address().port;
+        console.log('Express server listening on port %s.', port);
+    });
+
+});
+
 
 function handleMouseMove(event) {
 
@@ -27,9 +83,9 @@ function handleMouseMove(event) {
 
     event = event || window.event; // IE-ism
 
-    // IfpageX/Y aren't available and clientX/Y
+    // If pageX/Y aren't available and clientX/Y
     // are, calculate pageX/Y - logic taken from jQuery
-        // Calculate pageX/Y ifmissing and clientX/Y available
+        // Calculate pageX/Y if missing and clientX/Y available
     if(event.pageX == null && event.clientX != null) {
         eventDoc = (event.target && event.target.ownerDocument) || document;
         doc = eventDoc.documentElement;
@@ -45,90 +101,59 @@ function handleMouseMove(event) {
 
     sendEvent("cursorMotion", moment(), event.pageX, event.pageY, null, null);
 
-// create white dots to show where the cursor has been
-    // dot = document.createElement('div');
-    // dot.className = "dot";
-    // dot.style.left = event.pageX + "px";
-    // dot.style.top = event.pageY + "px";
-    // document.body.appendChild(dot);
+    // create white dots to show where the cursor has been
+    dot = document.createElement('div');
+    dot.className = "dot";
+    dot.style.left = event.pageX + "px";
+    dot.style.top = event.pageY + "px";
+    document.body.appendChild(dot);
 
-    // console.log( event.pageY, event.pageX)
+    console.log( event.pageY, event.pageX)
 
 }
 
-// Handler for internal server errors
-function errorHandler(err, req, res, next) {
-    console.error(err.message);
-    console.error(err.stack);
-    res.status(500).render('error_template', { error: err });
-}
 
 function sendEvent(eventType, eventTime, data1, data2, data3, data4){
-    // switch(eventType) {
-    //     case "cursorMotion":
+    switch(eventType) {
+        case "cursorMotion":
+            let evt = {
+                "type": "cursorMotion",
+                "x": data1,
+                "y": data2,
+                "time": moment()
+            } 
+    }
 
-    // }
-
-    let evt = {
-        "type": "cursorMotion",
-        "x": data1,
-        "y": data2,
-        "time": moment()
-    } 
 
     sessionEvents.push(evt);
     if (sessionEvents.length % 10 === 0) console.log(sessionEvents);
 }
 
-MongoClient.connect('mongodb://localhost:27017/a', function(err, db) {
-
-    assert.equal(null, err, "error connecting to MongoClient @ __dirname");
-    console.log("Successfully connected to MongoDB.");
-
-
-
-    app.get('/', function(req, res, next) {
-        var today = moment().date();
-        
-        // TODO: compare incoming user profile against preceding profiles
-        // if user exists, get id, insert visit date and duration to user profile
-        // else
-        // generate userID
-        userID = userID ? userID : shortid.generate();
-        
-        db.collection(today).insert({}).toArray(function(err, docs) {
-            res.render(today, { 'data': docs } );
-        });
-        res.render('index', { 'userID' : userID });
-
-    });
-
-    app.post('/movie-entered', function(req, res, next) {
-        var movie = {
-            "imdb": req.body.imdb,
-            "title": req.body.title,
-            "year": req.body.year
-        }
-        console.log(req.body);
-        console.log(movie, typeof movie.imdb, typeof movie.title, typeof movie.year, typeof movie);
-        if (typeof movie.imdb != 'string' || typeof movie.title != 'string' || typeof movie.year != 'string') {
-            next('Please fill out the form completely!');
-        }
-        else {
-            db.collection('movies').insert(movie);
-            res.sendFile('views/success.html', {root: __dirname });
-
-        }
-    });
+function eventHandling(){
     
-    app.use(errorHandler);
+    //Keyboard events
+    document.addEventListener('keydown', (event) => {
+        // const evt = "keydown";
+        const whichKey = event.key;
 
-    var server = app.listen(3000, function() {
-        var port = server.address().port;
-        console.log('Express server listening on port %s.', port);
-    });
+        console.log(`Key pressed ${whichKey}`);
+    }, false);
 
-});
+    document.addEventListener("keypress", (event) => {
+        console.log('keypress fired at:\n' + moment()  )
+    }, false); //[second]  
+
+    document.addEventListener("keyup", getEvtType, false); //third
+
+    //Mouse events
+    document.addEventListener("click", getEvtType, false); // third
+
+    document.addEventListener("mousedown", getEvtType, false); //first
+    document.addEventListener("mouseup", getEvtType, false); //second
+
+}
+
+
 
 
 
