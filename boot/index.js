7f7a82ee8eb17a99f3a85463f0f6199e45a254fe
Nathan White <nw@nwhite.net>
2018-01-10 11:32:21 -0800

    added boot loader to control app logic loading order

---
+++ boot/index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const errorHandler = require('errorhandler');
+const MongoClient = require('mongodb').MongoClient;
+
+const config = require('./config');
+const routes = require('../routes');
+
+const mongoConnStr = "mongodb://"
+    + `${process.env.MONGO_HOST || 'localhost'}`
+    + `:${process.env.MONGO_PORT || '27017'}`
+    + `/${process.env.MONGO_DB || 'giver'}`;
+
+module.exports = function boot (app) {
+
+    config(app);
+
+    MongoClient.connect('mongodb://localhost:27017/a', function(err, db) {
+
+        if (err) throw err;
+
+        app.db = db;
+
+        routes(app);
+
+        app.use(errorHandler({
+            dumpExceptions: app.enabled('debug'),
+            showStack: app.enabled('debug')
+        }));
+
+    });
+
+
+    return app;
+}
